21beb04bdbf012c042e4afb8d01a975e07f436a0
Francisco Daniel Freitas Martins <francisco.daniel@ambevtech.com.br>
2023-12-27 15:25:59 -0300

    defining Action Results

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,56 @@
+﻿using FirstAPI.Data;
+using FirstAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Controllers
+{
+    [ApiController]
+    [Route("api/produtos")]
+    public class ProdutosController : ControllerBase
+    {
+        private ApiDbContext _context { get; set; }
+
+        public ProdutosController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            return await _context.Produtos.FindAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProduto), new { id = produto.Id }, produto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutProduto(int id, Produto produto)
+        {
+            _context.Produtos.Update(produto);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{int:id}")]
+        public async Task<IActionResult> DeleteProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
