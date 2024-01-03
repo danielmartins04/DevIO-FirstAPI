1501dbbc30837dcc087984085cd4a2c79dbf622c
Francisco Daniel Freitas Martins <francisco.daniel@ambevtech.com.br>
2024-01-03 14:35:37 -0300

    add jwt config

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 using FirstAPI.Data;
+using FirstAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace FirstAPI
 {
@@ -26,6 +31,31 @@ namespace FirstAPI
             builder.Services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApiDbContext>();
+
+            // Pegando o token e gerando a chave encodada
+            var JwtSettingsSection = builder.Configuration.GetSection("JwTSettings");
+            builder.Services.Configure<JwtSettings>(JwtSettingsSection);
+
+            var jwtSettings = JwtSettingsSection.Get<JwtSettings>();
+            var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings.Audiencia,
+                    ValidIssuer = jwtSettings.Emissor
+                };
+            });
 
             var app = builder.Build();
 
