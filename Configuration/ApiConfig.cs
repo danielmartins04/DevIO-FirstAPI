7e1997cacc0ea220036bd158c7f9d7f298aed942
Francisco Daniel Freitas Martins <francisco.daniel@ambevtech.com.br>
2024-01-09 23:23:37 -0300

    structure otimization

---
+++ Configuration/ApiConfig.cs
@@ -0,0 +1,135 @@
+﻿using FirstAPI.Data;
+using FirstAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace FirstAPI.Configuration
+{
+    public static class ApiConfig
+    {
+        public static WebApplicationBuilder AddApiConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers()
+                            .ConfigureApiBehaviorOptions(options => {
+                                options.SuppressModelStateInvalidFilter = true;
+                            });
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            return builder;
+        }
+    }
+
+    public static class CorsConfig
+    {
+        public static WebApplicationBuilder AddCorsConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("Development", builder =>
+                            builder
+                                .AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader());
+
+                options.AddPolicy("Production", builder =>
+                            builder
+                                .WithOrigins("https://localhost:9000")
+                                .WithMethods("POST")
+                                .AllowAnyHeader());
+            });
+
+            return builder;
+        }
+    }
+
+    public static class SwaggerConfig
+    {
+        public static WebApplicationBuilder AddSwaggerConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return builder;
+        }
+    }
+
+    public static class IdentityConfig
+    {
+        public static WebApplicationBuilder AddIdentityConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApiDbContext>();
+
+            // Pegando o token e gerando a chave encodada
+            var JwtSettingsSection = builder.Configuration.GetSection("JwTSettings");
+            builder.Services.Configure<JwtSettings>(JwtSettingsSection);
+
+            var jwtSettings = JwtSettingsSection.Get<JwtSettings>();
+            var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = jwtSettings.Audiencia,
+                    ValidIssuer = jwtSettings.Emissor
+                };
+            });
+
+            return builder;
+        }
+    }
+
+    public static class DbContextConfig
+    {
+        public static WebApplicationBuilder AddDbContextConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<ApiDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return builder;
+        }
+    }
+}
